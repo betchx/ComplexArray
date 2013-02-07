54a066a26ee010ba2fd0cdede95cf51af0fb75dc
Atsushi TANABE <tanabe.atsushi@nikken.jp>
2013-02-08 00:29:48 +0900

    Add project and solution for Unit Test from C#.
    Unit test was very limit now.
    
    NUnit is used for unit test framework.
    The currently used NUnit is version 2.6.2 (latest stable).
    
    Test runner automatically lounched after sucessfull build.
    In order to execute test runner,
    the directory of nunit-x86.exe must be added into
     the PATH evnironment variables.

---
+++ TestComplexArray/TestComplex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ComplexArrayLib;
+namespace TestComplexArray
+{
+    using NUnit.Framework;
+
+    [TestFixture]    
+    public class TestComplex
+    {
+        [Test]
+        public void Construct()
+        {
+            Complex c = new Complex();
+            Assert.AreEqual(0.0, c.Real, 0.0001);
+            Assert.AreEqual(0.0, c.Imag, 0.0001);
+            const double e = 0.0001;
+            double r = 387;
+            double i = 83.32;
+            double v = 97.92;
+
+            c = new Complex(r,i);
+            Assert.AreEqual(r, c.Real, e);
+            Assert.AreEqual(i, c.Imag, e);
+
+            Complex d = new Complex(c);
+            Assert.AreEqual(r, d.Real, e);
+            Assert.AreEqual(i, d.Imag, e);
+
+            c = new Complex(v);
+            Assert.AreEqual(v, c.Real, e);
+            Assert.AreEqual(0.0, c.Imag, e);
+        }
+    }
+}
---
+++ TestComplexArray/TestComplexArray.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ComplexArrayLib;
+
+namespace TestComplexArray
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class TestComplexArray
+    {
+        [Test]
+        public void Abs()
+        {
+            int n = 100;
+            var arr = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = 1.0 + i;
+            }
+            ComplexArray carr = ComplexArray.real(arr);
+
+            var res = carr.abs();
+
+            for (int i = 0; i < n; i++)
+            {
+                Assert.AreEqual(arr[i], res[i], 0.0000001);
+            }
+
+            var r2 = Math.Sqrt(2.0);
+            carr.imag_assign(arr);
+            res = carr.abs();
+
+            for (int i = 0; i < n; i++)
+            {
+                Assert.AreEqual(arr[i]*r2, res[i], 0.001);
+            }
+
+        }
+
+        [Test]
+        public void Power()
+        {
+            int n = 100;
+            var arr = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = 1.0 + i;
+            }
+            ComplexArray carr = ComplexArray.real(arr);
+
+            var res = carr.power();
+            var ans = arr.Select(b => b * b).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                Assert.AreEqual(ans[i], res[i], 0.0000001);
+            }
+
+            carr.imag_assign(arr);
+            res = carr.power();
+
+            for (int i = 0; i < n; i++)
+            {
+                Assert.AreEqual(2*ans[i], res[i], 0.001);
+            }
+
+        }
+    }
+}
