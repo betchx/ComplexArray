132bbfac8954d1a1729269a5890c6ceca9ecea0a
tanabe <tanabe.atsushi@gmail.com>
2013-02-11 17:47:29 +0900

    Add Test of WaveData from C#

---
+++ TestComplexArray/TestWaveData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ComplexArrayLib;
+
+namespace TestComplexArray
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class TestWaveData
+    {
+        [Test]
+        public void Construction()
+        {
+            int n = 10;
+            WaveData wd = new WaveData(n);
+
+            Assert.AreEqual(n, wd.Length);
+
+            n = 24;
+            double[] arr = new double[n];
+            wd = new WaveData(arr);
+            Assert.AreEqual(n, wd.Length);
+
+            n = 20;
+            ComplexArray ca = new ComplexArray(n, false);
+            wd = new WaveData(ca);
+
+            Assert.AreEqual(n + 1, wd.SpLength);
+            Assert.AreEqual(n * 2, wd.Length);
+
+        }
+    }
+}
